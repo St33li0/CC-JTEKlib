21117d7f978422764eddbd4539b056d293f623fc
Jacob Steel <140410193+St33li0@users.noreply.github.com>
2023-09-28 14:39:14 +0100

    Added GeneratorControl.lua
    CC Control Script for integrating the ability to control a set of inputs that feed fuel into generators. Takes input from a minimum of two energy storage sources.

---
+++ Programs/GeneratorControl.lua
@@ -0,0 +1,74 @@
+monitor = peripheral.wrap("monitor_0")
+
+allDisplays = {
+    peripheral.wrap("monitor_0");
+    peripheral.wrap("monitor_1");
+}
+
+function writeDisplay(posX,posY,text)
+    for i=1, #allDisplays do
+        allDisplays[i].setCursorPos(posX,posY)
+        allDisplays[i].write(text)
+    end
+end
+
+writeDisplay(1,1,"Systems Online")
+writeDisplay(1,2,"Hopper Lock: ")
+writeDisplay(1,3,"Empty: ")
+writeDisplay(1,4,"Full: ")
+writeDisplay(1,5,"J-Tek Industries")
+
+function HopperLock(switch)
+    if switch == 1 then
+        if redstone.testBundledInput("bottom",colors.combine(colors.blue)) == false then
+            redstone.setBundledOutput("bottom",colors.combine(colors.blue))
+        end
+        writeDisplay(14,2,"ON ")
+    end
+    if switch == 0 then
+        if redstone.testBundledInput("bottom",colors.combine(colors.blue)) then
+            redstone.setBundledOutput("bottom",2^14)
+        end
+        writeDisplay(14,2,"OFF")
+    end
+end
+
+function GenInput(cycles)
+    local i = 0
+    while i < cycles do
+        HopperLock(0)
+        os.sleep(20)
+        HopperLock(1)
+        os.sleep(4800)
+    end
+end
+
+function Empty()
+    if redstone.testBundledInput("bottom",colors.red) == true then
+        GenInput(4)
+        writeDisplay(8,3,"TRUE ")
+    elseif redstone.testBundledInput("bottom",colors.red) == false then
+        writeDisplay(8,3,"FALSE")
+    end
+end
+
+function Full()
+    if redstone.testBundledInput("bottom",colors.yellow) == true then
+        writeDisplay(7,4,"TRUE ")
+    elseif redstone.testBundledInput("bottom",colors.yellow) == false then
+        writeDisplay(7,4,"FALSE")
+    end
+end
+
+HopperLock(1)
+Empty()
+Full()
+
+while true do
+    os.pullEvent("redstone")
+    if redstone.testBundledInput("bottom",colors.red) then
+        Empty()
+    elseif redstone.testBundledInput("bottom",colors.yellow) then
+        Full()
+    end
+end
