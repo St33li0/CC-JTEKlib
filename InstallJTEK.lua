fad2f375318ebcef04fdaf8719060b52ef1fd829
Jacob Steel <140410193+St33li0@users.noreply.github.com>
2024-01-12 10:07:33 +0000

    Fixed Install Script
    -=- Restructured entire function lib -=-
    Ln 41 - Changed i=0 to i=1 fixing the entire code.

--- InstallJTEK.lua
+++ InstallJTEK.lua
@@ -1,19 +1,57 @@
 term.clear()
 
 programs = {
-    "programs/GeneratorControl.lua"
-
+    "GeneratorControl.lua";
 }
 
-function copyAllFiles(path)
-    for i=1, #programs do
-        fs.copy(programs[i],path)
+
+
+installPath = "JTEK/"
+sourcePath = "disk/Programs/"
+
+function header()
+    -- JTEK Industries Header
+    clear(true,1,1)
+    print("JTEK Industries 2023")
+    print()
+end
+
+function runtime()
+    -- Runtime environment
+    os.sleep(1)
+    menu(1)
+end
+
+function writeScreen(posX,posY,text)
+    term.setCursorPos(posX,posY)
+    write(text)
+end
+
+function clear(all,posY)
+    if all == true then
+        term.clear()
+        term.setCursorPos(1,posY)
+    elseif all == false then
+        term.setCursorPos(1,posY)
+        term.clearLine()
     end
 end
 
-function copySomeFile(files,path)
-    for number in string.gmatch(files, '([^,]+)') do
-        fs.copy(programs[number],path)
+function copyAllFiles(path)
+    for i=1, #programs do
+        origin = sourcePath..programs[i]
+        destination = installPath..programs[i]
+        fs.copy(origin,destination)
+    end
+end
+
+function copySomeFiles(files,path)
+    matched = string.gmatch(files, '([^,]+)')
+    for number in matched do
+        number = tonumber(number)
+        origin = sourcePath..programs[i]
+        destination = installPath..programs[i]
+        fs.copy(origin,destination)
     end
 end
 
@@ -22,10 +60,10 @@ function install(all)
     if all == true then
         -- Run Install All to JTEK/
         if fs.exists("JTEK") then
-            copyAllFiles("JTEK")
+            copyAllFiles("JTEK/")
         else
             fs.makeDir("JTEK")
-            copyAllFiles("JTEK")
+            copyAllFiles("JTEK/")
         end
     elseif all == false then
         -- Run Menu for Install Choice
@@ -38,7 +76,7 @@ end
 
 function menu(menu)
     -- Menu for choosing what to install
-    if menu == 1 then
+    if menu == 1 then -- Ask to Install All
         header()
         while choice ~= "Y" and choice ~= "N" do
             clear(false,3)
@@ -50,27 +88,28 @@ function menu(menu)
         if choice == "Y" then
             install(true)
         else
-            install (false)
+            install(false)
         end
-    elseif menu == 2 then
+    elseif menu == 2 then -- If 1 == False, Ask which programs to install
         header()
         print("Please choose the programs you would like to install")
         print("Enter the numbers for all programs you wish to add to the install")
         print("Format: 1,2,3,4")
         print()
         for i=1, #programs do
-            write(i,". ",programs[i])
             term.setCursorPos(1,i+8)
+            write(tostring(i).."."..programs[i])
+            
         end
+        print()
         choice = read()
-        if fs.exists("JTEK") then
-            copySomeFiles(choice,"JTEK")
+        if fs.exists(installPath) then
+            copySomeFiles(choice,installPath)
         else
-            fs.makeDir("JTEK")
-            copySomeFiles(choice,"JTEK")
+            fs.makeDir(installPath)
+            copySomeFiles(choice,installPath)
         end
     end
-
 end
 
 function error(type)
@@ -90,41 +129,6 @@ function error(type)
     print("Program will restart")
     os.sleep(4)
     runtime()
-end
-
-function header()
-    -- JTEK Industries Header
-    clear(true,1,1)
-    print("JTEK Industries 2023")
-    print()
-end
-
-function runtime()
-    -- Runtime environment
-    os.sleep(1)
-    menu(1)
-end
-
-function writeDisplay(posX,posY,text)
-    for i=1, #allDisplays do
-        allDisplays[i].setCursorPos(posX,posY)
-        allDisplays[i].write(text)
-    end
-end
-
-function writeScreen(posX,posY,text)
-    term.setCursorPos(posX,posY)
-    write(text)
-end
-
-function clear(all,posY)
-    if all == true then
-        term.clear()
-        term.setCursorPos(posX,posY)
-    elseif all == false then
-        term.setCursorPos(1,posY)
-        term.clearLine()
-    end
 end
 
 runtime()
